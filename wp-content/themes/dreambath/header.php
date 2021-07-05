0ab88b8e6955308ea87644ff2599d1f49b6683a9
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-05 15:57:14 +0300

    ADDED | Header

---
+++ wp-content/themes/dreambath/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<?php wp_head(); ?>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <!-- Google Font -->
+
+
+    <!-- Css Styles -->
+    
+</head>
+
+<body <?php body_class(); ?> >
+			<?php wp_body_open(); ?>
+
+   
+   
+			<!-- Header Section Begin -->
+			<header class="header">
+						<div class="container-fluid">
+								<div class="row">
+							     <div class="top-header">
+							     <div class="col-md-7">
+											<div class="header_greetings">
+													<div class="header__logo">
+															<a href="./index.html"><img src="/wp-content/themes/dreambath/assets/images/QA-Logo-Navy.png" alt=""></a>
+														</div>
+														<div class="greetings_desc">
+															<div class="greetings_enter">
+																<h3>Enter to win!</h3>
+															</div>
+															<div class="greetings_numb">
+																<h2>$ 10,000</h2>
+															</div>
+															<div class="greetings_dream">
+																<h4>Dream bath makeover</h4>
+															</div>
+
+												</div>
+											</div>
+										</div>
+										<div class="col-md-5">		
+								       	<div class="header_phone">
+									        	<p>(757)722-bath</p>
+								        	</div>
+                    </div>
+									</div>
+
+						
+						</div>
+							<div class="bottom-header">
+
+							</div>
+										
+										
+								
+						</div>
+				</header>
+				<!-- Header Section End -->
