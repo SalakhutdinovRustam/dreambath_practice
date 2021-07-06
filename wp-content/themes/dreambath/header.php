ccb8dbf5b6c0033ba6855c3f8efa792ee0b8aab9
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-06 12:19:35 +0300

    UPDATED | Header

--- wp-content/themes/dreambath/header.php
+++ wp-content/themes/dreambath/header.php
@@ -16,18 +16,17 @@
     
 </head>
 
-<body <?php body_class(); ?> >
-			<?php wp_body_open(); ?>
+<!-- <body <?php body_class(); ?> >
+			<?php wp_body_open(); ?> -->
 
    
    
 			<!-- Header Section Begin -->
 			<header class="header">
-						<div class="container-fluid">
+						<div class="container">
 								<div class="row">
-							     <div class="top-header">
-							     <div class="col-md-7">
-											<div class="header_greetings">
+								     <div class="top-header ">
+							  		    <div class="header_greetings">
 													<div class="header__logo">
 															<a href="./index.html"><img src="/wp-content/themes/dreambath/assets/images/QA-Logo-Navy.png" alt=""></a>
 														</div>
@@ -42,24 +41,48 @@
 																<h4>Dream bath makeover</h4>
 															</div>
 
-												</div>
-											</div>
-										</div>
-										<div class="col-md-5">		
+														</div>
+													</div>
+										 		
+										
+											<div class="header_form">
 								       	<div class="header_phone">
-									        	<p>(757)722-bath</p>
+													 <a href="#">(757)722-bath</a>
+									        	
 								        	</div>
-                    </div>
-									</div>
+													
+                    		<div class="contact__content">
+                        	 <div class="contact__form">
+                           	 <h5>SEND MESSAGE</h5>
+                            	<form action="#">
+                               
+                           	 </form>
+                       		 </div>
+                      </div>
+											</div>
+                
+                     </div>
+										</div>
 
 						
-						</div>
-							<div class="bottom-header">
-
-							</div>
+						
+								 
 										
-										
+						</div>		
 								
-						</div>
-				</header>
+						
+			</header>
+			<div class="bottom-header">
+				<div class="discount">
+				<div class="discount_text">
+					<p>Next drawing:</p>
+				</div>
+			<div class="discount__countdown" id="countdown-time" data-final-date="2021/08/12">
+				<div class="countdown__item"><span>36</span> <br> <p>Day</p> </div>
+				<div class="countdown__item"><span>13</span><br> <p>Hour</p> </div>
+				<div class="countdown__item"><span>04</span><br> <p>Min</p> </div>
+				<div class="countdown__item"><span>03</span> <br><p>Sec</p> </div>
+		  </div>
+		 </div>
+		</div>
 				<!-- Header Section End -->
