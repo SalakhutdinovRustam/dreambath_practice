db69a994fe9ca60b1d8d088320821f13fe4a7f22
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-07 10:41:53 +0300

    UPDATED | Footer

---
+++ wp-content/themes/dreambath/footer.php
@@ -0,0 +1,29 @@
+<!-- Footer Section Begin -->
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ">
+							    <div class="footer__logo">
+									<a href="./index.html"><img src="/wp-content/themes/dreambath/assets/images/QA-Logo-Navy.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+								<div class="footer__widget">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+                    </div>
+                </div>
+            </div>
+          </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    
+    <!-- Search End -->
+
+    <!-- Js Plugins -->
+ 
+
+			<?php wp_footer(); ?>
+</body>
+
+</html>
