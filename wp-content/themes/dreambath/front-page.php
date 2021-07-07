3494ad2e7771ae7e0aded38bd535e8c789802cfa
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-07 10:45:32 +0300

    ADDED | Front -page

---
+++ wp-content/themes/dreambath/front-page.php
@@ -0,0 +1,71 @@
+<?php 
+
+get_header();
+?>
+    <!-- Base Section Begin -->
+    <section class="base_module">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-7 col-lg-8 m-auto ">
+									<div class="banner_title">
+										<h4>Here`s what you could win:</h4>
+									</div>
+								<div class="banner__slider owl-carousel">
+                    <div class="banner__item">
+                        <div class="banner__image">
+                            <img src="" alt="">
+                        </div>
+                    </div>
+                    <div class="banner__item">
+                        <div class="banner__image">
+												<img src="" alt="">
+                        </div>
+                    </div>
+                    <div class="banner__item">
+                        <div class="banner__image">
+												<img src="" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+		<div class="container">
+        <div class="row">
+            <div class="col-xl-7 col-lg-8 m-auto">
+						<div class="customer_title">
+										<h4>Our happy customers:</h4>
+                <div class="customer__slider owl-carousel">
+                    <div class="customer__item">
+                        <div class="customer__text">
+                            <span>The Chloe Collection</span>
+                            <h1>The Project Jacket</h1>
+                            <a href="#">Shop now</a>
+                        </div>
+                    </div>
+                    <div class="customer__item">
+                        <div class="customer__text">
+                            <span>The Chloe Collection</span>
+                            <h1>The Project Jacket</h1>
+                            <a href="#">Shop now</a>
+                        </div>
+                    </div>
+                    <div class="customer__item">
+                        <div class="customer__text">
+                            <span>The Chloe Collection</span>
+                            <h1>The Project Jacket</h1>
+                            <a href="#">Shop now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Base Section End -->
+
+
+
+<?php 
+get_footer();
+?>
