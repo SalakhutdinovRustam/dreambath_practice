4cae5150ae01ec5ede8bcd889aa17b96723976eb
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-08 09:43:26 +0300

    ADDED | OwlCarousel

--- wp-content/themes/dreambath/assets/scripts/main.js
+++ wp-content/themes/dreambath/assets/scripts/main.js
@@ -1,1 +1,27 @@
 import 'bootstrap/dist/js/bootstrap.bundle'
+import 'owl.carousel/dist/owl.carousel.min'
+
+/*--------------------------
+	    Banner Slider
+	----------------------------*/
+	$(".banner__slider").owlCarousel({
+		loop: true,
+		margin: 0,
+		items: 1,
+		dots: true,
+		smartSpeed: 1200,
+		autoHeight: false,
+		autoplay: false,
+		nav: true,
+	});
+
+	$(".customer__slider").owlCarousel({
+		loop: true,
+		margin: 0,
+		items: 1,
+		dots: true,
+		smartSpeed: 1200,
+		autoHeight: false,
+		autoplay: false,
+		nav: true,
+	});
