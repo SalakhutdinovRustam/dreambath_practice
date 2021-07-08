7960b61055a1da863db13096472cfd582c233630
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-08 09:39:09 +0300

    UPDATED | Front-page

--- wp-content/themes/dreambath/front-page.php
+++ wp-content/themes/dreambath/front-page.php
@@ -1,66 +1,93 @@
 <?php 
-
 get_header();
+
 ?>
     <!-- Base Section Begin -->
+<?php
+$slider_data = get_field('banner_slider');
+
+if (!empty($slider_data))
+	$banner_title = ($slider_data['banner_title']);
+	$slides = $slider_data['banner_slides'];
+//	var_dump($slides);
+	?>
+
+
+
     <section class="base_module">
         <div class="container">
             <div class="row">
-                <div class="col-xl-7 col-lg-8 m-auto ">
+                <div class="col m-auto ">
 									<div class="banner_title">
-										<h4>Here`s what you could win:</h4>
+										<h4><?php echo $banner_title ?></h4>
 									</div>
 								<div class="banner__slider owl-carousel">
+								<?php foreach ($slides as $slide): ?>
                     <div class="banner__item">
                         <div class="banner__image">
-                            <img src="" alt="">
+                            <img src="<?php echo $slide['banner_image']['url'] ?>" alt="">
                         </div>
                     </div>
-                    <div class="banner__item">
-                        <div class="banner__image">
-												<img src="" alt="">
-                        </div>
-                    </div>
-                    <div class="banner__item">
-                        <div class="banner__image">
-												<img src="" alt="">
-                        </div>
-                    </div>
+                    
+										<?php endforeach; ?>   
                 </div>
+								
             </div>
         </div>
     </div>
+
+		<?php
+$customer_slider_data = get_field('customer_slider');
+
+if (!empty($customer_slider_data))
+	$customer_title = ($customer_slider_data['customer_title']);
+	$customer_slides = $customer_slider_data['customer_slides'];
+
+	?>
 		<div class="container">
         <div class="row">
-            <div class="col-xl-7 col-lg-8 m-auto">
-						<div class="customer_title">
-										<h4>Our happy customers:</h4>
-                <div class="customer__slider owl-carousel">
+            <div class="col m-auto">
+						 <div class="customer_title"> 
+							<h4>Our happy customers:</h4> </div>
+							   <div class="customer__slider owl-carousel">
+								  <?php foreach ($customer_slides as $customer_slide): ?>
                     <div class="customer__item">
-                        <div class="customer__text">
-                            <span>The Chloe Collection</span>
-                            <h1>The Project Jacket</h1>
-                            <a href="#">Shop now</a>
+                       
+												<div class="rating-result">
+													<span class="active"></span>	
+													<span class="active"></span>    
+													<span class="active"></span>  
+													<span class="active"></span>    
+													<span class="active"></span>
+												</div>
+												<div class="customer__text">
+                            <h5><?php echo $customer_slide['consumer_feedback'] ?></h5>
+                            <p><?php echo $customer_slide['customer_name'] ?></p>
                         </div>
                     </div>
-                    <div class="customer__item">
-                        <div class="customer__text">
-                            <span>The Chloe Collection</span>
-                            <h1>The Project Jacket</h1>
-                            <a href="#">Shop now</a>
-                        </div>
-                    </div>
-                    <div class="customer__item">
-                        <div class="customer__text">
-                            <span>The Chloe Collection</span>
-                            <h1>The Project Jacket</h1>
-                            <a href="#">Shop now</a>
-                        </div>
-                    </div>
-                </div>
+										<?php endforeach; ?>
+                 </div>
             </div>
         </div>
     </div>
+		<?php	$button_url = get_field('button_donate');
+			$donate_title = get_field('donate_title');
+	//	var_dump($button_url);
+		?>
+		
+		<div class="container">
+        <div class="row">
+            <div class="col m-auto">
+						  <div class="support_block">
+							<div class="support_title"> 
+							<p><?php echo $donate_title ?></p> 
+						   </div>
+							 <button href=<?php echo $button_url ?> class="btn">Donate us</button>
+						     </div>
+                </div>
+						</div>
+				</div>
+
 </section>
 <!-- Base Section End -->
 
