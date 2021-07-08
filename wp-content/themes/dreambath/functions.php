296ca781bf383070fcb055e7521343826c4844ff
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-08 09:44:05 +0300

    ADDED | Options page

--- wp-content/themes/dreambath/functions.php
+++ wp-content/themes/dreambath/functions.php
@@ -5,3 +5,4 @@ define('THEME_DIR_URI', get_template_directory_uri());
 require_once THEME_DIR .'/inc/helpers/file.php';
 require_once THEME_DIR .'/inc/wp/enqueue_scripts.php';
 require_once THEME_DIR .'/inc/wp/theme_support.php';
+require_once THEME_DIR .'/inc/acf/options_page.php';
