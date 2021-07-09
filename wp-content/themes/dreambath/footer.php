2713fe6ffbf9df2592643eb192ba0e12becff910
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-07-09 10:32:33 +0300

    UPDATED | Footer

--- wp-content/themes/dreambath/footer.php
+++ wp-content/themes/dreambath/footer.php
@@ -1,15 +1,23 @@
+<?php	$acf_fields = get_field('footer', 'options');
+							
+$footer_logo = $acf_fields ['footer_logo']['url'];	
+$network_url = $acf_fields ['network_url'];	
+$icon_class = $acf_fields ['icon_class'];					
+							?>
 <!-- Footer Section Begin -->
 <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 ">
+
+							
 							    <div class="footer__logo">
-									<a href="./index.html"><img src="/wp-content/themes/dreambath/assets/images/QA-Logo-Navy.png" alt=""></a>
+									<a href="./index.html"><img src="<?php echo $footer_logo ?>" alt=""></a>
                     </div>
                 </div>
                 <div class="col-lg-6">
 								<div class="footer__widget">
-								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="<?php echo $network_url ?>"><i class="<?php echo $icon_class ?>"></i></a>
                     </div>
                 </div>
             </div>
